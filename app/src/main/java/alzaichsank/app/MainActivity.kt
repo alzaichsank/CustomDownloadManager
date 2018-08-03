ac43c386dfcc42a9953d1ebbc742ed4608481460
Alza Devloper <alzaichsank@gmail.com>
2018-08-03 21:34:20 +0700

    AK: add dummy link

--- app/src/main/java/alzaichsank/app/MainActivity.kt
+++ app/src/main/java/alzaichsank/app/MainActivity.kt
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var link_doc : String = "http://codehopedevloper.com/dummy_downloader/docx_exemple_download.docx"
+    private var link_pdf : String = "http://codehopedevloper.com/dummy_downloader/pdf_exemple_download.pdf"
+    private var link_ppt : String = "http://codehopedevloper.com/dummy_downloader/ppt_exemple_download.pptx"
+    private var link_xls : String = "http://codehopedevloper.com/dummy_downloader/xls_exemple_downlod.xlsx"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
